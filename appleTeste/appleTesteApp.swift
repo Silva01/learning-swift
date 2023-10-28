bdba71fcb1b48c87cec4155137039ab94b3b63cf
silva01 <silva20102@gmail.com>
2023-10-28 18:06:08 -0300

    Initial Commit

---
+++ appleTeste/appleTesteApp.swift
@@ -0,0 +1,17 @@
+//
+//  appleTesteApp.swift
+//  appleTeste
+//
+//  Created by Daniel Silva on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct appleTesteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
